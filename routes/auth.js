807c94c4302508108af586c1473a24ef415d3927
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-02-02 11:33:54 +0630

    RegisterUpdated

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,26 @@
-const path = require('path');
-const express = require('express');
-const rootDir = require('../util/path');
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
 const router = express.Router();
 
-router.get('/signup', (req, res, next) => {
-    res.render('signup', {
-        pageTitle: 'SignUp',
-        path: '/signup'
-    });
+router.get("/signup", (req, res, next) => {
+  res.render("signup", {
+    pageTitle: "SignUp",
+    path: "/signup",
+  });
 });
 
-router.get('/login', (req, res, next) => {
-    res.render('login', {
-        pageTitle: 'Login',
-        path: '/login'
-    });
+router.get("/login", (req, res, next) => {
+  res.render("login", {
+    pageTitle: "Login",
+    path: "/login",
+  });
 });
 
-// router.post('/signup', (req, res, next) => {
-//     res.redirect('/login');
-// });
+router.post("/login", (req, res) => {});
+
+router.post("/register", (req, res, next) => {
+  res.send("Register");
+});
 
 module.exports = router;
