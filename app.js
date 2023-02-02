42208587e2c92044bb28d6dce53503c3c7384d0e
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-02-02 11:16:23 +0630

    Route updated

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authRoute = require("./models/auth");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const addpostData = require('./routes/add-post');
-const indexRoutes = require('./routes/index');
-const authRoutes = require('./routes/auth');
+const addpostData = require("./routes/add-post");
+const indexRoutes = require("./routes/index");
+const authRoutes = require("./routes/auth");
 
 // const userModels = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', addpostData.routes);
+app.use("/admin", addpostData.routes);
 app.use(indexRoutes);
 app.use(authRoutes);
 // app.use('/user', userModels);
+app.use("/api/user", authRoute);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
-
 mongoose
-    .connect('mongodb+srv://itvisionhubs:itvisionhub0001xpl@cluster01.kzbsplu.mongodb.net/errorhandling')
-    .then(result => {
-        app.listen(8080, console.log('Do your job right now!'));
-    })
-    .catch(err => console.log(err));
+  .connect(
+    "mongodb+srv://itvisionhubs:itvisionhub0001xpl@cluster01.kzbsplu.mongodb.net/errorhandling"
+  )
+  .then((result) => {
+    app.listen(8080, console.log("Do your job right now!"));
+  })
+  .catch((err) => console.log(err));
